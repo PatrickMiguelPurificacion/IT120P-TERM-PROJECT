5ea274586a4a500c2822d086ce04a7f9febc93b9
RyAr <johnpulgar52902@gmail.com>
2023-03-30 20:13:35 +0800

    Changed to Vite JS and Firebase for Database

---
+++ src/scripts/firebase-init.ts
@@ -0,0 +1,21 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyCWJtEFkuQb9BK4jH1WKI0osyKnXnlB4mE",
+  authDomain: "fabric-finesse.firebaseapp.com",
+  projectId: "fabric-finesse",
+  storageBucket: "fabric-finesse.appspot.com",
+  messagingSenderId: "1032571613019",
+  appId: "1:1032571613019:web:04ebb31cf21b89e3b7747e",
+  measurementId: "G-5H2M7EDQLL"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
