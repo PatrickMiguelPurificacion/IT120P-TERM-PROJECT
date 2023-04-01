70f784dec6e32eb4f1a3c72e51ca6ba402e3a133
JChanFromGit <chan.james91@yahoo.com>
2023-04-01 21:42:25 +0800

    added employee log page

--- src/components/Employee Log/employeeLog.tsx
+++ src/components/Employee Log/employeeLog.tsx
@@ -1,20 +1,78 @@
 import './employeeLog.css'
+import avatar from "./avatar.webp"
+
+function buttonClick() {
+  alert('You\'ve clicked the button!');
+}
 
 const EmployeeLog = () => {
     return (
         <div>
+          <div className="wrapper">
         <div className="sidebar">
-            <span className='brand-name'>Fabric Finesse</span>
-          <a className="active" href="#home">Home</a>
-          <a href="#news">News</a>
-          <a href="#contact">Contact</a>
-          <a href="#about">About</a>
+            <span className='brand-name'><b>Fabric</b><b>Finesse</b></span>
+          <a className="active" href="">Home</a>
+          <a href="#">Profile</a>
+          <a href="#">Orders</a>
+          <a href="#">Log</a>
+          <a href="#">Stocks</a>
         </div>
+        <div className="navbar">
+            <h1>EMPLOYEE LOGIN PAGE</h1>
+            </div>
+            </div>
+
         <div className="content">
-          <h2>Responsive Sidebar Example</h2>
-          <p>This example use media queries to transform the sidebar to a top navigation bar when the screen size is 700px or less.</p>
-          <p>We have also added a media query for screens that are 400px or less, which will vertically stack and center the navigation links.</p>
-          <h3>Resize the browser window to see the effect.</h3>
+          <div className="profile-picture">
+            <img src={avatar}></img>
+          </div>
+
+          <form className="form-inline">
+        <label htmlFor="email">Employee ID</label>
+        <input type="email" id="employeeID" placeholder="" name="employeeID" />
+        <label htmlFor="pwd">Name</label>
+        <input type="email" id="name" placeholder="" name="name" />
+        <label htmlFor="email">Email</label>
+        <input type="email" id="email" placeholder="" name="email" />
+        <label htmlFor="pwd">Role</label>
+        <input type="email" id="role" placeholder="" name="role" />
+
+        <label htmlFor="email">Contact Number</label>
+        <input type="email" id="pnumber" placeholder="" name="pnumber" />
+        <label htmlFor="pwd">Address</label>
+        <input type="email" id="address" placeholder="" name="address" />
+
+        <input type="button" className="button" onClick={buttonClick} defaultValue="SUBMIT" />
+      </form>
+
+      <div className="table-responsive">
+        <table>
+        <tr>
+        <th>Date</th>
+      <th>Start Time</th>
+      <th>End Time</th>
+        </tr>
+
+        <tr>
+        <td>08/12/2012</td>
+      <td>Jackson</td>
+      <td>13:49</td>
+        </tr>
+
+        <tr>
+        <td>08/12/2012</td>
+      <td>Jackson</td>
+      <td>13:49</td>
+        </tr>
+
+        <tr>
+        <td>08/12/2012</td>
+      <td>Jackson</td>
+      <td>13:49</td>
+        </tr>
+        </table>
+
+      </div>
         </div>
         </div>
     )
