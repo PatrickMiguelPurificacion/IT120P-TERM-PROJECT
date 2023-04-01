a0c4f67521f3ffb624ed9d33615d6451a6ee2c34
JChanFromGit <chan.james91@yahoo.com>
2023-04-01 17:07:11 +0800

    added sidebar

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './index.css'
-import EmployeeLog from './Employee Log/employeeLog'
+import EmployeeLog from './components/Employee Log/employeeLog'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
