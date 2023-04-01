d74cd2d134ac9c147ee9fd7186ea9a00374c119a
Ernest <benitezernest33@gmail.com>
2023-04-01 15:27:57 +0800

    added employee log component

---
+++ src/components/Employee Log/employeeLog.tsx
@@ -0,0 +1,13 @@
+import './employeeLog.css'
+
+const EmployeeLog = () => {
+    return (
+        <div>
+            <div className="background">
+                <h1>hello world!</h1>
+            </div>
+        </div>
+    )
+}
+
+export default EmployeeLog
--- src/components/main.tsx
+++ src/components/main.tsx
@@ -1,10 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App'
 import './index.css'
+import EmployeeLog from './Employee Log/employeeLog'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    <EmployeeLog />
   </React.StrictMode>,
 )
