95d973d9bb7fd2832b8371420eb31f78332dea36
JJMYao <jjmyao@mymail.mapua.edu.ph>
2023-04-14 17:26:11 +0800

    Updated Services Page

--- src/components/services/Services.tsx
+++ src/components/services/Services.tsx
@@ -4,28 +4,28 @@ function Services(){
 
     const services = [
         {
-            Name: "SERVICE 1",
-            Description: "Desc 1",
-            Procedure: "Procedure 1",
-            Delivery: "Truck",
-            ImageUrl: "../src/assets/temp-image.jpg",
-            Price: 100.00
+            Name: "PICKUP & DELIVERY",
+            Description: "These services allow you to schedule a set pickup time for your laundry. You can have it picked up from your home, your work, or any other location that is within our service area, making it easy to get your laundry sent in when you need to, even if you don’t have the time to drop it off.",
+            Procedure: "Put simply, a pick-up and delivery laundry service does what it says on the tin. Your local laundromat will collect your dirty laundry and bring it back to you after giving it a thorough wash, dry, and fold. Some added costs are involved compared to completing your own laundry or heading into the laundromat yourself, but many people find the extra spend worthwhile.",
+            Delivery: "Laundry-Truck",
+            ImageUrl: "../src/assets/services/Page2.png",
+            Price: 300.00
         },
         {
-            Name: "SERVICE 2",
-            Description: "Desc 3",
-            Procedure: "Procedure 3",
-            Delivery: "Motor",
-            ImageUrl: "../src/assets/temp-image.jpg",
-            Price: 1000.00
+            Name: "DRY CLEANING SERVICE",
+            Description: "Dry cleaning is a professional laundry service that uses solvents instead of water to clean clothes made from delicate materials. This process is gentle on fabrics and removes stains without causing damage. Laundry businesses offer this service to customers who need to clean suits, dresses, and other delicate clothing items.",
+            Procedure: "The dry cleaning process typically involves inspecting the garment for stains or damage, pre-treating any stains, and then placing the garment in a machine that uses solvents to clean it. Once the cleaning cycle is complete, the garment is dried and pressed, and then inspected again for any remaining stains or damage. Finally, the garment is packaged and returned to the customer.",
+            Delivery: "Car",
+            ImageUrl: "../src/assets/services/Page3.png",
+            Price: 800.00
         },
         {
-            Name: "SERVICE 3",
-            Description: "Desc 3",
-            Procedure: "Procedure 3",
+            Name: "ONLINE SERVICE",
+            Description: "An online laundry service is a convenient solution for people who have busy lifestyles and need to outsource their laundry tasks. It allows customers to schedule laundry pickups and drop-offs at their preferred times and locations, often through a mobile app or website. The laundry is then cleaned, folded, and returned to the customer's doorstep, saving them time and effort.",
+            Procedure: "To use an online laundry service, customers typically start by signing up for an account and selecting the services they need, such as washing, drying, folding, and ironing. They then schedule a pickup time and location, often through a mobile app or website. The laundry is picked up by a delivery person, taken to a cleaning facility, and then returned to the customer's specified location within a designated timeframe.",
             Delivery: "Ferry",
-            ImageUrl: "../src/assets/temp-image.jpg",
-            Price: 10000.00
+            ImageUrl: "../src/assets/services/Page4.png",
+            Price: 500.00
         }
     ]
 
@@ -40,9 +40,9 @@ function Services(){
                         <div className="col-md-6">
                             <h1 className="text-center">Types of Services</h1>
                             <ul className="ms-5 lead mt-5">
-                                <li>Service 1</li>
-                                <li>Service 2</li>
-                                <li>Service 3</li>
+                                <li>Pickup and Delivery</li>
+                                <li>Dry Cleaning Service</li>
+                                <li>Online Service</li>
                             </ul>
                             <h1 className="text-center pt-5 mt-5">AVAILABLE ORDERS</h1>
                             <div className="text-center p-5">
@@ -52,7 +52,7 @@ function Services(){
                             </div>
                         </div>
                         <div className="col-md-6 text-center">
-                            <img src="../src/assets/temp-image.jpg" />
+                            <img src="../src/assets/services/Page1.png" />
                         </div>
                     </div>
                 </div>
@@ -68,7 +68,7 @@ function Services(){
                                 <div className="row text-light">
                                     <div className="col-md-4 text-center">
                                         <p className="display-5 mt-5">PHP{item.Price}</p>
-                                        <img src="../src/assets/temp-image.jpg" width="80%" />
+                                        <img src={item.ImageUrl} width="80%" />
                                         <button className="btn btn-primary mt-5 p-3">Create Order</button>
                                     </div>
                                     <div className="col-md-8">
@@ -117,7 +117,7 @@ function Services(){
                                     </div>
                                     <div className="col-md-4 text-center">
                                         <p className="display-5 mt-5">PHP{item.Price}</p>
-                                        <img src="../src/assets/temp-image.jpg" width="80%" />
+                                        <img src={item.ImageUrl} width="80%" />
                                         <button className="btn btn-primary mt-5 p-3">Create Order</button>
                                     </div>
                                 </div>
